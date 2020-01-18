24bdd2d4d0bca167366e475c71dd21e3bf8a5e72
Rodrigo Felix <rodrigo.felix321@gmail.com>
2020-01-18 14:35:34 -0300

    Finalizado capítulo 9 - Arrays e Exceptions

--- src/br/com/casadocodigo/livraria/produtos/Livro.java
+++ src/br/com/casadocodigo/livraria/produtos/Livro.java
@@ -1,6 +1,7 @@
 package br.com.casadocodigo.livraria.produtos;
 
 import br.com.casadocodigo.livraria.Autor;
+import br.com.casadocodigo.livraria.exception.AutorNuloException;
 
 public abstract class Livro implements Produto{
 
@@ -26,6 +27,9 @@ public abstract class Livro implements Produto{
 	
 	public Livro(Autor autor) {
 		this();
+		if (autor == null) {
+			throw new AutorNuloException("O Autor do Livro não pode ser nulo");
+		}
 		this.autor = autor;
 	}
 	
--- src/br/com/casadocodigo/livraria/teste/CarrinhoDeCompras.java
+++ src/br/com/casadocodigo/livraria/teste/CarrinhoDeCompras.java
@@ -1,17 +1,27 @@
 package br.com.casadocodigo.livraria.teste;
 
+
 import br.com.casadocodigo.livraria.produtos.Produto;
 
 public class CarrinhoDeCompras {
 	
 	private double total;
+	private Produto[] produtos = new Produto[10];
+	private int contador = 0;
 	
 	public void adiciona(Produto produto) {
 		System.out.println("Adicionando: " + produto);
-		total += produto.getValor();
+		this.produtos[1] = produto;
+		contador ++;
+		this.total += produto.getValor();
 	}
 	
+	public Produto[] getProdutos() {
+		return produtos;
+	}
+
 	public double getTotal() {
 		return total;
 	}
+	
 }
